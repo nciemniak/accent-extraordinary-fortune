17b34a4459bcddcc6a486db420e772048e947d0c
nciemniak <nciemniak@gmail.com>
2024-02-22 14:57:33 +0100

    base template, css starting point and posting birthday data

--- app/views.py
+++ app/views.py
@@ -1,7 +1,18 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 
+# testing
+import pdb
+# end testing
+
 def index(request):
     context = { "test": "test text" }
     return render(request, "app/index.html", context)
-    #return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def zodiac_animal(request):
+    birthday = request.POST["birthday"]
+    breakpoint()
+
+    context = { "test": "test text" }
+    return render(request, "app/zodiac_animal.html", context)
