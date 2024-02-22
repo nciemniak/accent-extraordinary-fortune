a4f30753f7049bedbec3c33f4bdbd2141c0de58a
nciemniak <nciemniak@gmail.com>
2024-02-22 14:57:49 +0100

    add 'zodiac animal' page to urls

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "app"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("zodiac_animal", views.zodiac_animal, name="zodiac_animal"),
 ]
