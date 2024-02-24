48ecdf1e2fd8b2819bd2820f8c4b9f6c917505b6
nciemniak <nciemniak@gmail.com>
2024-02-24 15:01:49 +0100

    data + view logic updates

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class Zodiac(models.Model):
   year = models.IntegerField(default=0)
@@ -6,3 +7,12 @@ class Zodiac(models.Model):
   animal = models.CharField(max_length=200)
   start_date = models.DateField()
   end_date = models.DateField()
+
+  @property
+  def image_url(self):
+    return f"app/images/zodiac_animals/{self.element}_{self.animal}.jpeg"
+  
+  @property
+  def title(self):
+    text = f"{self.element} {self.animal}!"
+    return text.upper()
--- app/views.py
+++ app/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.utils.dateparse import parse_date
+
+from datetime import datetime
+
+from .models import Zodiac
 
 # testing
 import pdb
@@ -12,6 +17,7 @@ def index(request):
 
 def zodiac(request):
     birthday = request.POST["birthday"]
-
-    context = { "test": "test text" }
-    return render(request, "app/zodiac.html", context)
+    birthday_parsed = parse_date(birthday)
+    
+    zodiac = Zodiac.objects.filter(start_date__lte=birthday_parsed, end_date__gte=birthday_parsed).first()
+    return render(request, "app/zodiac.html", {"zodiac": zodiac})
